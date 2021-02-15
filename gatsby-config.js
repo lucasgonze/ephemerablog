e9ccac0b917e9ef66579d66588591e0c4cbf22bd
Lucas Gonze <lucas@gonze.com>
2021-02-15 13:17:10 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Ephemerablog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Lucas Gonze`,
+      summary: `Occam's Barber`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `A blog with only one page ever.`,
+    siteUrl: `https://ephemerablog.vercel.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `lucasgonze`,
     },
   },
   plugins: [
